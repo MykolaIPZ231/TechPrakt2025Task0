cb9fd5511a5604b755133e8888835ba5d7c1fa16
Mykola Ustymenko <ipz231_umo@student.ztu.edu.ua>
2025-02-04 19:52:53 +0200

    console app aded

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.Write("num 1 - ");
+            int.TryParse(Console.ReadLine(), out a);
+
+            Console.Write("num 2 - ");
+            int.TryParse(Console.ReadLine(), out b);
+
+            sum = a + b;
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
